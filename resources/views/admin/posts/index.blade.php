d5eeed5e3c07dace1bc5e993db9b630a9c7a59bc
Spectrum5 <toskusergio@gmail.com>
2023-03-22 02:53:36 +0100

    Creata table su index per mostrare tutti i posts

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -2,21 +2,15 @@
 
 @section('content')
     <div class="container-fluid mt-4">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">{{ __('Posts') }}</div>
+        <div class="row justify-content-center md-4">
+            <div class="col">
+                <h1>
+                    Tutti gli articoli
+                </h1>
 
-                    <div class="card-body">
-                        @if (session('status'))
-                            <div class="alert alert-success" role="alert">
-                                {{ session('status') }}
-                            </div>
-                        @endif
-
-                        Tutti gli articoli
-                    </div>
-                </div>
+                <a href="{{ route('admin.posts.create') }}" class="btn btn-success">
+                    Aggiungi articolo    
+                </a>
             </div>
         </div>
 
@@ -32,10 +26,20 @@
             <tbody>
                 @foreach ($posts as $post)
                     <tr>
-                        <th scope="row">1</th>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
+                        <th scope="row">{{ $post->id }}</th>
+                        <td>{{ $post->title }}</td>
+                        <td>{{ $post->slug }}</td>
+                        <td>
+                            <a href="{{ route('admin.posts.show', $post->id) }}" class="btn btn-primary">
+                                Dettagli
+                            </a>
+                            <a href="{{ route('admin.posts.show', $post->id) }}" class="btn btn-warning">
+                                Aggiorna
+                            </a>
+                            <a href="{{ route('admin.posts.show', $post->id) }}" class="btn btn-danger">
+                                Elimina
+                            </a>
+                        </td>
                     </tr>
                 @endforeach
             </tbody>
