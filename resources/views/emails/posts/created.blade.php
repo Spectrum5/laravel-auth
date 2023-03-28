02a0af9d9be23b0f3feae43a677fd6f3df29e910
Spectrum5 <toskusergio@gmail.com>
2023-03-28 05:38:17 +0200

    Fine esercizio - prova style email

--- resources/views/emails/posts/created.blade.php
+++ resources/views/emails/posts/created.blade.php
@@ -1,4 +1,28 @@
-<div>
+<!DOCTYPE html>
+<html lang="it">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Email per nuovo post</title>
+
+        {{-- Style --}}
+
+        <style>
+            h1 {
+                color: red;
+            }
+
+            p, span {
+                color: lightcoral;
+            }
+            span {
+                font-weight: bold;
+            }
+        </style>
+    </head>
+    <body>
+        <div>
     <h1>
         Nuovo post pubblicato!
     </h1>
@@ -16,3 +40,7 @@
         </li>
     </ul>
 </div>
+    </body>
+</html>
+
+
